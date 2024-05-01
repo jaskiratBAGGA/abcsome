faf9a1f81fd602de41a6b5a2e3b6018d7f2bb2bd
jaskirat_bagga <111983923+jaskiratBAGGA@users.noreply.github.com>
2024-05-01 23:06:59 +0530

    Add files via upload

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,44 +6,53 @@ import numpy as np
 import torch
 
 app = Flask(__name__)
-
 model_path = os.path.join(".", "wights_raw.pickle")
 scaler_path = os.path.join(".", "scaler.pkl")
-
 print(f"Loading model from: {model_path}")
 print(f"Loading scaler from: {scaler_path}")
-
 model, meta_data = load_model(model_path)
 scaler = joblib.load(scaler_path)
-
 class_mapping = {0: "Normal", 1: "Pre-Failure", 2: "Failure"}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction = None
+    error_message = None
     if request.method == 'POST':
-        input_data = [float(x) for x in request.form['input_data'].split(',')]
-        prediction = make_prediction(input_data)
-    return render_template('index.html', prediction=prediction)
+        input_data = request.form['input_data'].split(',')
+        try:
+            input_data = [float(x) for x in input_data]
+            prediction = make_prediction(input_data)
+            if isinstance(prediction, str):
+                error_message = prediction
+        except ValueError:
+            error_message = "Please enter 16 comma-separated numerical values."
+    return render_template('index.html', prediction=prediction, error_message=error_message)
 
 def make_prediction(data):
-    data_t = np.array(data, dtype=np.float32)
-    data_t = scaler.transform(data_t.reshape(-1, 16))
-    data_t = torch.tensor(data_t, dtype=torch.float32)
-    preds = model(data_t).detach().cpu().numpy()[0]
-    epred = np.exp(preds)
-    probs = epred / epred.sum()
-    pclass = np.argmax(probs)
-    conf = np.max(probs)
-    lbl = class_mapping[int(pclass)]
-    prediction = ModelPrediction(
-        predicted_class=lbl,
-        predicted_label=int(pclass),
-        raw_out=preds.tolist(),
-        probabilities=probs.tolist(),
-        confidence=conf
-    )
-    return prediction
+    try:
+        data_t = np.array(data, dtype=np.float32)
+        if data_t.shape[0] != 16:
+            return "Please enter 16 comma-separated values."
+
+        data_t = scaler.transform(data_t.reshape(-1, 16))
+        data_t = torch.tensor(data_t, dtype=torch.float32)
+        preds = model(data_t).detach().cpu().numpy()[0]
+        epred = np.exp(preds)
+        probs = epred / epred.sum()
+        pclass = np.argmax(probs)
+        conf = np.max(probs)
+        lbl = class_mapping[int(pclass)]
+        prediction = ModelPrediction(
+            predicted_class=lbl,
+            predicted_label=int(pclass),
+            raw_out=preds.tolist(),
+            probabilities=probs.tolist(),
+            confidence=conf
+        )
+        return prediction
+    except ValueError:
+        return "Invalid input. Please enter 16 comma-separated values."
 
 @app.route('/Model', methods=['GET'])
 def get_model():
